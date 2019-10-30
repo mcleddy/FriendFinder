7c602b9a0ef5177c7a0afc2b732ec4fc1ab2d783
mcleddy <maryleddy1@gmail.com>
2019-10-29 22:00:05 -0700

    first writing

---
+++ server.js
@@ -0,0 +1,20 @@
+//dependencies
+const express = require("express");
+const path =require("path");
+const bodyParser =require("body-parser");
+
+//create server set up port
+const port=process.env.PORT || 8080;
+const app =express;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+//listen to port
+app.listen(port, ()=> console.log("listening at port ", port))
